7fdd5ab033210806e1c0e7ed42c22b032bb5e822
uriasvitor <uriasvitor@gmail.com>
2023-12-23 00:39:07 -0300

    Add: Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HighlightComponent } from './highlight/highlight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    HighlightComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      inter: '"Inter", sans-serif',
+      'jockey-one':['Jockey One','sans-serif']
+    },
+    screens:{
+      'full': '100%'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
