8a6fc3776a508cb7de444ebd4a2ba90c54db8cdb
uriasvitor <uriasvitor@gmail.com>
2023-12-24 17:24:31 -0300

    add:mobile added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,7 +10,9 @@ export class NavbarComponent {
 
   toggleMobileMenu(): void {
     const mobileMenu = document.getElementById('mobile-menu');
+    const navbar = document.getElementsByClassName('navbar').item(0);
     mobileMenu?.classList.toggle('hidden');
+    navbar?.classList.toggle('menu-is-open');
     mobileMenu?.classList.toggle('full-screen');
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
