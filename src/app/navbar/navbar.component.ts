a035f5ec2405c4b31f0aee8a9195c15d064db99a
uriasvitor <uriasvitor@gmail.com>
2023-12-24 00:45:29 -0300

    add:Menu icon to header

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  toggleMobileMenu(): void {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu?.classList.toggle('hidden');
+  }
 }
